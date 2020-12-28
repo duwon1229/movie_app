0a4fc52d33d469f0eb7e424350b3bdb606843ee8
Keunyoung-Jung <46441769+Keunyoung-Jung@users.noreply.github.com>
2020-12-28 21:14:33 +0900

    map Recpa edu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import { computeHeadingLevel } from '@testing-library/react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: 
+      "https://admin.cjrecipe.com:9007/images/theKitchen/PHON/0000001651/0000006094/0000001651.jpg"
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://recipe1.ezmember.co.kr/cache/recipe/2015/05/22/e1af3baa780984f52bedf67991b49c081.jpg"
+  }
+];
+
+function Food({name, picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} alt={name} />
+  </div>;
 }
 
+function App() {
+  return( 
+  <div>
+    {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))}
+  </div> 
+    );
+}
 export default App;
