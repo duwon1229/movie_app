2aaab14b09ab65fe80aaa7da915d9f9cf1fea18a
Keunyoung-Jung <46441769+Keunyoung-Jung@users.noreply.github.com>
2020-12-28 23:35:02 +0900

    how to use prop-types

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 // import { computeHeadingLevel } from '@testing-library/react';
 import React from 'react';
+import PropTypes from "prop-types";
+// import { findAllByDisplayValue } from '@testing-library/react';
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
     image: 
-      "https://admin.cjrecipe.com:9007/images/theKitchen/PHON/0000001651/0000006094/0000001651.jpg"
+      "https://admin.cjrecipe.com:9007/images/theKitchen/PHON/0000001651/0000006094/0000001651.jpg",
+    rating: 2.6
   },
   {
     id: 2,
     name: "Samgyeopsal",
     image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2015/05/22/e1af3baa780984f52bedf67991b49c081.jpg"
+      "https://recipe1.ezmember.co.kr/cache/recipe/2015/05/22/e1af3baa780984f52bedf67991b49c081.jpg",
+    rating: 1
   }
 ];
 
-function Food({name, picture}) {
+function Food({name, picture, rating}) {
   return <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>;
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
 function App() {
   return( 
   <div>
-    {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))}
+    {foodILike.map(dish => (<Food key={dish.id} 
+    name={dish.name} picture={dish.image} rating={dish.rating} />))}
   </div> 
     );
 }
